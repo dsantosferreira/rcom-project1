58063f6436679e9c70aba33aaf7bc471856d0e76
Mansur <mustafin.mansur02@gmail.com>
2023-09-20 23:40:47 +0100

    logic connection establishment

--- write_noncanonical.c
+++ write_noncanonical.c
@@ -19,23 +19,32 @@
 #define FALSE 0
 #define TRUE 1
 
-#define FRAME_SIZE 5
+#define BUF_SIZE 5
 
-#define FLAG 0x7E
-#define SET 0x03
-#define UA 0x07
-#define SS_AR 0x03
-#define SR_AS 0x01
+#define FLAG    0x7E
+#define A_SEND  0x03
+#define A_RECV  0x01
+#define C_SET   0x03
+#define C_UA    0x07
 
 volatile int STOP = FALSE;
 
-int main(int argc, char *argv[])
-{
-    // Program usage: Uses either COM1 or COM2
+int check_ua(unsigned char *answer){
+    if(answer[0] == FLAG &&
+        answer[1] == A_RECV && 
+        answer[2] == C_UA &&
+        answer[3] == A_RECV ^ C_UA && 
+        answer[4] == FLAG)
+    return TRUE;
+    return FALSE;
+}
+
+
+int main(int argc, char *argv[]){
+
     const char *serialPortName = argv[1];
 
-    if (argc < 2)
-    {
+    if (argc < 2){
         printf("Incorrect program usage\n"
                "Usage: %s <SerialPort>\n"
                "Example: %s /dev/ttyS1\n",
@@ -48,8 +57,7 @@ int main(int argc, char *argv[])
     // because we don't want to get killed if linenoise sends CTRL-C.
     int fd = open(serialPortName, O_RDWR | O_NOCTTY);
 
-    if (fd < 0)
-    {
+    if (fd < 0){
         perror(serialPortName);
         exit(-1);
     }
@@ -58,8 +66,7 @@ int main(int argc, char *argv[])
     struct termios newtio;
 
     // Save current port settings
-    if (tcgetattr(fd, &oldtio) == -1)
-    {
+    if (tcgetattr(fd, &oldtio) == -1){
         perror("tcgetattr");
         exit(-1);
     }
@@ -87,68 +94,45 @@ int main(int argc, char *argv[])
     tcflush(fd, TCIOFLUSH);
 
     // Set new port settings
-    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-    {
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1){
         perror("tcsetattr");
         exit(-1);
     }
 
     printf("New termios structure set\n");
 
+
     // Create string to send
-    unsigned char buf[FRAME_SIZE] = {FLAG, SS_AR, SET, SS_AR ^ SET, FLAG};
-
-    // In non-canonical mode, '\n' does not end the writing.
-    // Test this condition by placing a '\n' in the middle of the buffer.
-    // The whole buffer must be sent even with the '\n'.
-    // buf[5] = '\n';
-
-    int bytes = write(fd, buf, FRAME_SIZE);
+    unsigned char buf[BUF_SIZE] = {0};
+    buf[0] = FLAG;
+    buf[1] = A_SEND;
+    buf[2] = C_SET;
+    buf[3] = buf[1] ^ buf[2];
+    buf[4] = FLAG;
+    
+    int bytes;
+    if((bytes = write(fd, buf, BUF_SIZE)) < 0){
+        perror("Error write send command");
+        exit(-1);
+    }
     printf("%d bytes written\n", bytes);
-    
-    unsigned char read_frame[FRAME_SIZE + 1] = {0};
-    unsigned char read_buf[1];
-    int second_flag = FALSE; // Checks if second flag is the next to be read
-	int idx = 0;
-    
-    while (STOP == FALSE) {
-		int bytes = read(fd, read_buf, 1);
-		
-		if (bytes == -1) {
-			perror("read");
-			exit(-1);
-		}
-		
-		read_frame[idx++] = read_buf[0];
-		
-		if (read_buf[0] == FLAG) {
-			if (second_flag) {
-				STOP = TRUE;
-			}
-			else {
-				second_flag = TRUE;
-			}
-		}
-	}
-	
-	printf("var = 0x%02X\n", read_frame[0]);
-	printf("var = 0x%02X\n", read_frame[1]);
-	printf("var = 0x%02X\n", read_frame[2]);
-	printf("var = 0x%02X\n", read_frame[3]);
-	printf("var = 0x%02X\n", read_frame[4]);
-	
-	int bcc = read_frame[1] ^ read_frame[2];
-	
-	if (bcc == read_frame[3]) {
-		printf("Connection established\n");
-	}
-	else {
-		printf("Error detected in frame that was received\n");
-	}
 
     // Wait until all bytes have been written to the serial port
     sleep(1);
 
+    while (STOP == FALSE)
+    {
+        int bytes = read(fd, buf, BUF_SIZE);
+
+        if(check_ua(buf)){
+            printf("Connection established\n");
+            STOP = TRUE;
+        }else{
+            perror("Incorrect UA command recive");
+            exit(-1);
+        }
+    }
+    
     // Restore the old port settings
     if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
     {
