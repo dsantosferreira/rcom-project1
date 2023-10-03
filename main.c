5c41a28bee605cd69e37f2b1aa54afedd0b68f0e
Mansur <mustafin.mansur02@gmail.com>
2023-10-04 00:53:19 +0100

    added error handler

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "data_link/data_link.h"
 
 
-// Argumentos
-// 1: /dev/ttySxx
-// 2: r ; s
 int main(int argc, char *argv[])
 {   
     if (argc < 3)
     {
-        return -1;
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS10 s\n",
+               argv[0],
+               argv[0]);
+        exit(-1);
     }
-    unsigned char flag_f;
+
+    int r;
     const char *serialPort = argv[1];
     const char *flag = argv[2];
-    if(flag[0] == 'r'){
-        flag_f = 1;
-    }
-    if(flag[0] == 's'){
-        flag_f = 0;
-    }
-    int r = llopen(serialPort, flag_f);
+    if(flag[0] == 'r') r = llopen(serialPort, RECEIVER);
+    if(flag[0] == 's') r = llopen(serialPort, TRANSMITTER);
+    
     if(r < 0) printf("Some error !\n");
+
     return 0;
 }
+
